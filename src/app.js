c0bff90f19b5864c5e311240f0205dd1cf062162
Parul <parulagarwal1605@gmail.com>
2025-01-03 19:06:45 +0530

    6Epiosde

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 const express = require("express");
+
+
+const connectDb= require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-app.use("/",(req, res)=>{
-    res.send("hi")
+// app.use("/",(req, res)=>{
+//     res.send("hi")
 
-})
+// })
 
-app.use("/test",(req, res)=>{res.send("babu")
+app.post("/signup",async(req,res)=>{
+    //Creating a new instance of the user model
+   const user= new User({
+        firstName:"Bijay",
+        lastName:"agarwal",
+        emailId:"juw",
+        gender:"female",
+        age:9
+    })
+    await user.save();
+    res.send("success");
 })
-app.listen(3000,()=>{
-    console.log("listening to 3000")
+connectDb().then(()=>{
+    console.log("connection done");
+
+app.listen(7777,()=>{
+    console.log("listening to 7000")
 });
+})
+.catch((err)=>{
+    console.error("db cannot be connected")
+})
