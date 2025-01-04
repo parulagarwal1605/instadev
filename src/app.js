8cdcc9eb7d4b561494689fab53714ed7b7858c4d
Parul <parulagarwal1605@gmail.com>
2025-01-04 12:02:25 +0530

    Ep7firstCommit

--- src/app.js
+++ src/app.js
@@ -9,18 +9,28 @@ const app = express();
 //     res.send("hi")
 
 // })
+//if we send body from postman and try to console it it will not support for reading json and converting it into js object we need middleware 
+//express provide espress.json as middleware
+app.use(express.json());
 
 app.post("/signup",async(req,res)=>{
+    console.log("hi",req.body);
     //Creating a new instance of the user model
-   const user= new User({
-        firstName:"Bijay",
-        lastName:"agarwal",
-        emailId:"juw",
-        gender:"female",
-        age:9
-    })
-    await user.save();
-    res.send("success");
+//    const user= new User({
+//         firstName:"ijay",
+//         lastName:"agarwal",
+//         emailId:"juw",
+//         gender:"female",
+//         age:9
+//     })
+//     try{
+//         await user.save();
+//         res.send("success");
+//     }catch(error){
+//         res.status(400).send("d",error)
+      
+//     }
+   
 })
 connectDb().then(()=>{
     console.log("connection done");
